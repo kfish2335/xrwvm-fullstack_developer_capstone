75765c230ba22f7c313b431b8afc84f4f3b5e8d5
KurkF <kurk.fisher@outlook.com>
2024-04-22 19:31:11 -0400

    week 5 update 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,8 @@
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils.timezone import now
+
+# from django.utils.timezone import now
 
 # Create your models here.
 
